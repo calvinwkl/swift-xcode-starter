6947838a6964dc61d7e4fdb3fd1a71c9fd18f847
Calvin Wong <kalokwong9@gmail.com>
2018-06-09 01:59:05 +0800

    init project with .gitignore

---
+++ starter/starter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  starter
+//
+//  Created by Wong Ka Lok on 9/6/2018.
+//  Copyright © 2018 Wong Ka Lok. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
