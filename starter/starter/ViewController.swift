c99e04749e39391dfb41ea9da2f74f7bc55ed6c2
Calvin Wong <kalokwong9@gmail.com>
2018-06-09 02:01:03 +0800

    Add set background color

--- starter/starter/ViewController.swift
+++ starter/starter/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // You can set background color here or in storyboard
+        view.backgroundColor = .blue
     }
 
     override func didReceiveMemoryWarning() {
