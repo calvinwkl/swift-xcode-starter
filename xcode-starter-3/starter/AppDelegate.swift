03ec26b14a3e4553a1d18a6fcfdb8f8ca73d0ec4
Calvin Wong <kalokwong9@gmail.com>
2018-06-17 01:56:12 +0800

    Add xcode-starter-3, cocoapods project

---
+++ xcode-starter-3/starter/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  starter
+//
+//  Created by Wong Ka Lok on 9/6/2018.
+//  Copyright © 2018 Wong Ka Lok. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+}
+
---
+++ xcode-starter-3/starter/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  starter
+//
+//  Created by Wong Ka Lok on 9/6/2018.
+//  Copyright © 2018 Wong Ka Lok. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        // You can set background color here or in storyboard
+        view.backgroundColor = .blue
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
